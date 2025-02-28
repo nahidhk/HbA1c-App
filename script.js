1f90a829a9958d5dea5c5f6e7b10eca58d5f4483
nahidhk <nahidappltd@gmail.com>
2025-02-28 11:27:49 +0600

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//
+const method = document.getElementById('step1');
+const dataValue = document.getElementById('step2');
+const mmolShow = document.getElementById('mmoll');
+function tester(){
+  const mmolData = 18;
+  if (method.value === "mg/dL") {
+    const mmolview = dataValue.value/mmolData;
+   mmolShow.textContent= mmolview.toFixed(2);
+  }
+}
